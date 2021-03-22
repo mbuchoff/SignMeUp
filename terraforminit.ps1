0d7dae5d1d8eb9341fa343a2c6edad8804b7192a
Michael Buchoff <mbuchoff@gmail.com>
2021-03-22 09:53:31 -0400

    Started terraforming resources

---
+++ terraforminit.ps1
@@ -0,0 +1,2 @@
+$Env:ARM_SAS_TOKEN='?sv=2020-02-10&ss=b&srt=sco&sp=rwdlacx&se=2121-03-19T21:08:17Z&st=2021-03-19T13:08:17Z&spr=https&sig=hf6Sd1oT7Qw9kBhq1i5QiT7sXYEYwsigYuR8iliZ6hg%3D'
+terraform init
