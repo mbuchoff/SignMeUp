4903cf0bd0ddef55c5249674b61b8b5998b86902
Michael Buchoff <mbuchoff@gmail.com>
2021-03-30 09:54:13 -0400

    Cleaned up UI a bit, added schedules

--- SignMeUp/Vendor/VendorSite/wwwroot/ts/site.ts
+++ SignMeUp/Vendor/VendorSite/wwwroot/ts/site.ts
@@ -1,5 +1,6 @@
 ﻿import Calendar from 'tui-calendar';
 import "tui-calendar/dist/tui-calendar.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 // If you use the default popups, use this.
 import 'tui-date-picker/dist/tui-date-picker.css';
@@ -8,6 +9,7 @@ import 'tui-time-picker/dist/tui-time-picker.css';
 var calendar = new Calendar('#calendar', {
     defaultView: 'month',
     taskView: true,
+    useCreationPopup: true,
     template: {
         monthDayname: function (dayname: any) {
             return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
