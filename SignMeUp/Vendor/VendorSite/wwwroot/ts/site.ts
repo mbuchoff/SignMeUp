6b98becbe5348e80df05f071ec8547c3f8dc3d8a
Michael Buchoff <mbuchoff@gmail.com>
2021-04-02 09:40:39 -0400

    User can create a calendar event

--- SignMeUp/Vendor/VendorSite/wwwroot/ts/site.ts
+++ SignMeUp/Vendor/VendorSite/wwwroot/ts/site.ts
@@ -1,4 +1,4 @@
-﻿import Calendar from 'tui-calendar';
+﻿import Calendar, { ISchedule } from 'tui-calendar';
 import "tui-calendar/dist/tui-calendar.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -6,13 +6,35 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
 
-var calendar = new Calendar('#calendar', {
-    defaultView: 'month',
-    taskView: true,
+var cal = new Calendar('#calendar', {
     useCreationPopup: true,
-    template: {
-        monthDayname: function (dayname: any) {
-            return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
-        }
-  }
+    defaultView: 'week', // set 'week' or 'day'
+    taskView: true,  // e.g. true, false, or ['task', 'milestone'])
+    scheduleView: ['time']  // e.g. true, false, or ['allday', 'time'])
+});
+
+cal.createSchedules([
+    {
+       // id: '1',
+    //    calendarId: '1',
+        title: 'my schedule',
+        category: 'time',
+  //      dueDateClass: '',
+        start: '2021-04-02T22:30:00+09:00',
+        end: '2021-04-03T22:30:00+09:00',
+    },
+    {
+     //   id: '2',
+   //     calendarId: '1',
+        title: 'second schedule',
+        category: 'time',
+//        dueDateClass: '',
+        start: '2021-04-01T22:30:00+09:00',
+        end: '2021-04-02T22:30:00+09:00',
+    }
+]);
+
+cal.on('beforeCreateSchedule', function (event: ISchedule) {
+    event.category = 'time';
+    cal.createSchedules([event]);
 });
