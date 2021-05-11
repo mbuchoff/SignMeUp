a918d5cbedff2e4e7acc930733700cfe04551cda
Michael Buchoff <mbuchoff@gmail.com>
2021-05-11 08:48:52 -0400

    Removed connection string from repo

--- SignMeUp/Vendor/Database/VendorDbContext.cs
+++ SignMeUp/Vendor/Database/VendorDbContext.cs
@@ -6,11 +6,5 @@ namespace Database
     public class VendorDbContext : DbContext
     {
         public DbSet<Schedule> Schedules { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=sql-signmeup-vendor-dev-centralus.database.windows.net;Database=db-signmeup-vendor-dev-centralus;User ID=vendor_app_service;Password=0MK%mjwcA_VVvMTnV#iKJFnrC0IT0HEW;Trusted_Connection=False;Encrypt=True;");
-            base.OnConfiguring(optionsBuilder);
-        }
     }
 }
